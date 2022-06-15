7e1169bf21447d01a1b44d4b8fee559f819a7846
Saurabh Kumar <saurabh_kumar1@intuit.com>
2022-06-15 18:18:08 +0530

    feat: updated resume

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var icon = document.getElementById("icon");
-icon.onclick = function() {
-    document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-        icon.src = "images/sun.png";
-    } else {
-        icon.src = "images/moon.png";
-    }
-}
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+};
+
+var typed = new Typed("#typed_text", {
+  strings: ["Software Engineer", "ReactJS Developer", "Front-End Developer"],
+  smartBackspace: true,
+  backSpeed: 50,
+  typeSpeed: 50,
+  loop: true,
+  startDelay: 1000,
+  cursorChar: "",
+});
